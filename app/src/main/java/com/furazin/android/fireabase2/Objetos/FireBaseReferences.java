f8dbc117d23cea6408d7f79b171d6962d94e8236
Furacin <manzanile@gmail.com>
2017-03-09 02:11:04 +0100

    first commit. Se puede leer y escribir en la BD con Firebase.

---
+++ app/src/main/java/com/furazin/android/fireabase2/Objetos/FireBaseReferences.java
@@ -0,0 +1,10 @@
+package com.furazin.android.fireabase2.Objetos;
+
+/**
+ * Created by manza on 09/03/2017.
+ */
+
+public class FireBaseReferences {
+    final public static String USUARIOS_REFERENCE = "usuarios";
+    final public static String USUARIO_REFERENCE = "usuario";
+}
